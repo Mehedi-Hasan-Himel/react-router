1c0b2a00686ff6a7b1f77298e646cefac1427cd2
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-08-21 12:59:28 +0600

    ?

--- src/Components/FriendDetail/FriendDetail.js
+++ src/Components/FriendDetail/FriendDetail.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Friend from "../Friend/Friend";
 
 const FriendDetail = () => {
-   const params = useParams()
+  const { friendId } = useParams();
+  const [friend, setFriend] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
   return (
     <div>
-        <h2>This is details about bondhu : {params.friendId}</h2>
+      <h2>This is details about bondhu : {friendId}</h2>
+      <h3>Name: {friend.name}</h3>
+      <h4>Email: {friend.email}</h4>
+      <h6>Website: {friend.website}</h6>
+      <p>
+        <small>City: {friend.address?.city}</small>
+      </p>
+      <p>
+        <small>lat: {friend.address?.geo?.lat}</small>
+      </p>
     </div>
   );
 };
